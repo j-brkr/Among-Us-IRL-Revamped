f1aa12f0fa631d94cfc2f96ada15a8292a2b6702
Joel Brooker <joeltbrooker4@gmail.com>
2024-07-03 12:36:48 +0800

    Added sign-in link

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,69 @@
 from flask import render_template, redirect, url_for
 from app import app
 
+users = [
+    {
+        "name": "Lisa",
+        "colour": "#FF8EBAA0"
+    },
+    {
+        "name": "Anna",
+        "colour": "#FF8EBAA0"
+    },
+    {
+        "name": "Annie",
+        "colour": "#FF8EBAA0"
+    },
+    {
+        "name": "Benjamin",
+        "colour": "#FF8EBAA0"
+    },
+    {
+        "name": "Connor",
+        "colour": "#FF8EBAA0"
+    },
+    {
+        "name": "Jeff",
+        "colour": "#FF8EBAA0"
+    },
+    {
+        "name": "Joelle",
+        "colour": "#0000FFA0"
+    },
+    {
+        "name": "Nabla",
+        "colour": "#FF8EBAA0"
+    },
+    {
+        "name": "Sigh",
+        "colour": "#FF8EBAA0"
+    },
+    {
+        "name": "Sophy",
+        "colour": "#FF8EBAA0"
+    },
+    {
+        "name": "Felisha",
+        "colour": "#FF8EBAA0"
+    },
+    {
+        "name": "Cherry",
+        "colour": "#FF8EBAA0"
+    },
+    {
+        "name": "Ericknash",
+        "colour": "#FF8EBAA0"
+    },
+    {
+        "name": "Freyah",
+        "colour": "#FF8EBAA0"
+    },
+    {
+        "name": "Hailee",
+        "colour": "#FF8EBAA0"
+    }
+]
+
 @app.route('/')
 @app.route('/index')
 def index():
@@ -8,70 +71,8 @@ def index():
 
 @app.route('/select_user')
 def select_user():
-    users = [
-        {
-            "name": "Lisa",
-            "colour": "#FF8EBAA0"
-        },
-        {
-            "name": "Anna",
-            "colour": "#FF8EBAA0"
-        },
-        {
-            "name": "Annie",
-            "colour": "#FF8EBAA0"
-        },
-        {
-            "name": "Benjamin",
-            "colour": "#FF8EBAA0"
-        },
-        {
-            "name": "Connor",
-            "colour": "#FF8EBAA0"
-        },
-        {
-            "name": "Jeff",
-            "colour": "#FF8EBAA0"
-        },
-        {
-            "name": "Joelle",
-            "colour": "#0000FFA0"
-        },
-        {
-            "name": "Nabla",
-            "colour": "#FF8EBAA0"
-        },
-        {
-            "name": "Sigh",
-            "colour": "#FF8EBAA0"
-        },
-        {
-            "name": "Sophy",
-            "colour": "#FF8EBAA0"
-        },
-        {
-            "name": "Felisha",
-            "colour": "#FF8EBAA0"
-        },
-        {
-            "name": "Cherry",
-            "colour": "#FF8EBAA0"
-        },
-        {
-            "name": "Ericknash",
-            "colour": "#FF8EBAA0"
-        },
-        {
-            "name": "Freyah",
-            "colour": "#FF8EBAA0"
-        },
-        {
-            "name": "Hailee",
-            "colour": "#FF8EBAA0"
-        }
-    ]
     return render_template("select_user.html", title="Select User", users=users)
 
-@app.route('/sign-in')
-def sign_in():
-    return "Sign in page"
+@app.route('/sign_in/<username>')
+def sign_in(username):
+    return "Sign in page for " + username
