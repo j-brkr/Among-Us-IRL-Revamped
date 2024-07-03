f55ff13c21e29648a8bf2b55f1d20204c07aea61
Joel Brooker <joeltbrooker4@gmail.com>
2024-07-03 12:55:37 +0800

    Replaced internal spelling of colour to color

--- app/routes.py
+++ app/routes.py
@@ -4,63 +4,63 @@ from app import app
 users = [
     {
         "name": "Lisa",
-        "colour": "#FF8EBAA0"
+        "color": "#FF8EBAA0"
     },
     {
         "name": "Anna",
-        "colour": "#FF8EBAA0"
+        "color": "#FF8EBAA0"
     },
     {
         "name": "Annie",
-        "colour": "#FF8EBAA0"
+        "color": "#FF8EBAA0"
     },
     {
         "name": "Benjamin",
-        "colour": "#FF8EBAA0"
+        "color": "#FF8EBAA0"
     },
     {
         "name": "Connor",
-        "colour": "#FF8EBAA0"
+        "color": "#FF8EBAA0"
     },
     {
         "name": "Jeff",
-        "colour": "#FF8EBAA0"
+        "color": "#FF8EBAA0"
     },
     {
         "name": "Joelle",
-        "colour": "#0000FFA0"
+        "color": "#0000FFA0"
     },
     {
         "name": "Nabla",
-        "colour": "#FF8EBAA0"
+        "color": "#FF8EBAA0"
     },
     {
         "name": "Sigh",
-        "colour": "#FF8EBAA0"
+        "color": "#FF8EBAA0"
     },
     {
         "name": "Sophy",
-        "colour": "#FF8EBAA0"
+        "color": "#FF8EBAA0"
     },
     {
         "name": "Felisha",
-        "colour": "#FF8EBAA0"
+        "color": "#FF8EBAA0"
     },
     {
         "name": "Cherry",
-        "colour": "#FF8EBAA0"
+        "color": "#FF8EBAA0"
     },
     {
         "name": "Ericknash",
-        "colour": "#FF8EBAA0"
+        "color": "#FF8EBAA0"
     },
     {
         "name": "Freyah",
-        "colour": "#FF8EBAA0"
+        "color": "#FF8EBAA0"
     },
     {
         "name": "Hailee",
-        "colour": "#FF8EBAA0"
+        "color": "#FF8EBAA0"
     }
 ]
 
@@ -77,6 +77,6 @@ def select_user():
 def sign_in(username):
     joelle = {
         "name": "Joelle",
-        "colour": "#0000FFA0"
+        "color": "#0000FFA0"
     }
     return render_template("sign_in.html", title="Sign In", user=joelle)
