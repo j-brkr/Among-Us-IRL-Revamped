d802565879dc052a836c5a8a6003e131067b8597
Joel Brooker <joeltbrooker4@gmail.com>
2024-07-05 10:54:59 +0800

    Sign in PIN front end

--- app/static/enter_id.js
+++ app/static/enter_id.js
@@ -1,18 +1,53 @@
-const buttonText = ["1","2","3","4","5","6","7","8","9","X","0","V"];
+const buttonText = ["1","2","3","4","5","6","7","8","9","X","0","✓"];
+let pinInput = "";
+let pinDisplay = "";
 
 // When the document is loaded
 $( document ).ready(function() {
     console.log( "ready!" );
-    let table = $("<table></table>");
-    let i = 0;
-    for(let rowID=0; rowID<4; rowID++){
-        let row = $("<tr></tr>");
-        for(let colID=0; colID<3; colID++){
-            let cell = $("<td></td>").text(buttonText[i]);
-            row.append(cell)
-            i++;
-        }
-        table.append(row);
+    // Numpad
+    let numpad = $("<div></div>").attr("id", "numpad");
+    for(let i=0; i<12; i++){
+        let cell = $("<div></div>").text(buttonText[i]).attr("class", "cell");
+        cell.attr("id", "pin-"+buttonText[i]);
+        cell.on( "click", buttonFunc(buttonText[i]));
+        numpad.append(cell);
+        
     }
-    $('#screen').append(table);
+    $('#screen').append(numpad);
+    // PIN Box
+    let pinBox = $("<div></div>").attr("id", "pin-box");
+    $('#screen').append(pinBox);
 });
+
+function appendPinbox(c){
+    return function(){
+        pinBox = $('#pin-box');
+        if(pinBox.text().length < 4){
+            pinInput+= c;
+            pinDisplay+= "*"
+        }
+        pinBox.text(pinDisplay);
+    }
+}
+
+function buttonFunc(name){
+    if(!isNaN(parseInt(name))){ // If a digit
+        return appendPinbox(name);
+    }
+    else if(name === "X"){
+        return function clearPinbox(){
+            pinInput = "";
+            pinDisplay = "";
+            $('#pin-box').text(pinDisplay);
+        }
+    }else if(name === "✓"){
+        return function submitNumber(){
+            alert(pinInput);
+        }
+    }
+
+    return function(){
+        alert( "Handler for " + name + " called." );
+    }
+}
