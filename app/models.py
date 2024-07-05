dcf9534dffffdd22abeabb70d6513d151bd39a8a
Joel Brooker <joeltbrooker4@gmail.com>
2024-07-05 16:57:01 +0800

    code column replaced with pin column

--- app/models.py
+++ app/models.py
@@ -1,12 +1,17 @@
 import sqlalchemy as sa
 import sqlalchemy.orm as so
-from app import db
+from flask_login import UserMixin
+from app import db, login
 
-class User(db.Model):
+@login.user_loader
+def load_user(id):
+    return db.session.get(User, int(id))
+
+class User(UserMixin, db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
     name: so.Mapped[str] = so.mapped_column(sa.String(32))
     color: so.Mapped[str] = so.mapped_column(sa.String(7))
-    code: so.Mapped[str] = so.mapped_column(sa.String(4))
+    pin: so.Mapped[str] = so.mapped_column(sa.String(4))
 
     def __repr__(self):
         return "<User {}>".format(self.name)
@@ -18,7 +23,7 @@ User:
 - id
 - name
 - color
-- code
+- pin
 - icon_src
 
 Task:
