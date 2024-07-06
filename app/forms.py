66571f540456331dfb509030703cadd155109808
Joel Brooker <joeltbrooker4@gmail.com>
2024-07-06 15:15:18 +0800

    Made role reveal default and removed long cooldown times

--- app/forms.py
+++ app/forms.py
@@ -4,11 +4,11 @@ from wtforms.validators import DataRequired, Length
 
 class settingsForm(FlaskForm):
     imposter_count = IntegerField("# of Imposters", [DataRequired(), Length(min = 1, max = 10)], default = 3)
-    reveal_role = BooleanField("Reveal roles on death?")
+    reveal_role = BooleanField("Reveal roles on death?", default=True)
     emergency_meetings = IntegerField("# of Emergency Meetings", [DataRequired(), Length(min = 0, max = 10)], default = 3)
     discussion_time = SelectField("Discussion Time", [DataRequired()], choices=[(60,"1m"),(120,"2m"),(180,"3m"),(240,"4m"),(300,"5m"),(360,"6m"),(480,"8m"),(600,"10m"),(900,"15m"),(1200,"20m"),(86400,"all day")], default = 300)
-    emergency_cooldown = SelectField("Emergency Cooldown", [DataRequired()], choices=[(0,"0s"),(5,"5s"),(10,"10s"),(15,"15s"),(30,"30s"),(45,"45s"),(60,"60s"),(300,"5m")], default = 30)
-    kill_cooldown = SelectField("Kill Cooldown", [DataRequired()], choices=[(0,"0s"),(5,"5s"),(10,"10s"),(15,"15s"),(30,"30s"),(45,"45s"),(60,"60s"),(300,"5m")], default = 30)
+    emergency_cooldown = SelectField("Emergency Cooldown", [DataRequired()], choices=[(0,"0s"),(5,"5s"),(10,"10s"),(15,"15s"),(30,"30s"),(45,"45s"),(60,"60s")], default = 30)
+    kill_cooldown = SelectField("Kill Cooldown", [DataRequired()], choices=[(0,"0s"),(5,"5s"),(10,"10s"),(15,"15s"),(30,"30s"),(45,"45s"),(60,"60s")], default = 30)
     short_tasks = IntegerField("Short Tasks", [DataRequired(), Length(min = 0, max = 10)], default = 2)
     long_tasks = IntegerField("Long Tasks", [DataRequired(), Length(min = 0, max = 10)], default = 2)
     common_tasks = IntegerField("Common Tasks", [DataRequired(), Length(min = 0, max = 3)], default = 1)
