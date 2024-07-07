1c6095a99a21a8f08c33a54e97ca00011d88382b
Joel Brooker <joeltbrooker4@gmail.com>
2024-07-07 14:51:13 +0800

    Added game model and allow pins to be up to 6 digits long in the database

--- app/models.py
+++ app/models.py
@@ -11,35 +11,30 @@ class User(UserMixin, db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
     name: so.Mapped[str] = so.mapped_column(sa.String(32))
     color: so.Mapped[str] = so.mapped_column(sa.String(7))
-    pin: so.Mapped[str] = so.mapped_column(sa.String(4))
+    pin: so.Mapped[str] = so.mapped_column(sa.String(6))
 
     def check_pin(self, pin):
         return self.pin == pin
 
     def __repr__(self):
         return "<User {}>".format(self.name)
+    
+class Game(db.Model):
+    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+    active: so.Mapped[bool]
+    status: so.Mapped[str] = so.mapped_column(sa.String(32))
+    time_started: so.Mapped[int]
+    time_finished: so.Mapped[int]
 
-'''
-Plan
+    imposter_count: so.Mapped[int]
+    reveal_role: so.Mapped[bool]
+    emergency_meetings: so.Mapped[int]
+    discussion_time: so.Mapped[int]
+    emergency_cooldown: so.Mapped[int]
+    kill_cooldown: so.Mapped[int]
+    short_tasks: so.Mapped[int]
+    long_tasks: so.Mapped[int]
+    common_tasks: so.Mapped[int]
 
-User:
-- id
-- name
-- color
-- pin
-- icon_src
-
-Task:
-- id
-- name
-- type
-- weight?
-- room_id (fk)
-
-Room:
-- id
-- name
-
-GameLog (json)
-
-'''
+    def __repr__(self):
+        return "<Game {} {}>".format(self.id, ("Active" if self.active else ""))
