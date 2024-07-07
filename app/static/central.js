714aad5d72fdd5dcf2dbb795c998e304b88cf936
Joel Brooker <joeltbrooker4@gmail.com>
2024-07-07 17:58:47 +0800

    Added basic lobby

--- app/static/central.js
+++ app/static/central.js
@@ -1,7 +1,35 @@
 
 $( document ).ready(function(){
     $.get( "/api/game", function(data){
-        console.log(data);
-    });
+        let game = JSON.parse(data);
+        console.log(game);
+        changePage(game["status"]);
 
+    })
+    .fail(function(){
+        console.log("GET Failed")
+        document.write("GET Failed. Maybe there is no active game")
+    });
 })
+
+function changePage(status){
+    $( ".page" ).css("display", "none");
+    switch(status) {
+        case "LOBBY":
+            $( "#lobby" ).css("display", "block");
+            $( "body" ).css("background-color", "black");
+            break;
+        case "GAME":
+            $( "#emergencyButton" ).css("display", "block");
+            $( "body" ).css("background-color", "blue");
+            break;
+        case "VOTING":
+            $( "#voting" ).css("display", "block");
+            $( "body" ).css("background-color", "blue");
+            break;
+        default:
+            console.log("Status " + status + " not recognized");
+            break;
+}
+}
+
